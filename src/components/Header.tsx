e2f8fcfd678e097d2d619ffca761d943420c36c1
Vladislav Portnov <dkflbr-09@mail.ru>
2022-03-08 22:17:19 +0300

    reload

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+	return (
+		<header>
+			<div>test</div>
+			<div>new</div>
+			<div>header</div>
+		</header>
+	);
+};
+
+export default Header;
